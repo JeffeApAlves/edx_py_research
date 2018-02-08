96c3cee079b05245af7c04c2476d3d518028e65f
Jefferson Alves <jeffealves@gmail.com>
2018-02-08 11:35:44 -0200

    encoding2 via string

--- HW3.py
+++ HW3.py
@@ -1,10 +1,11 @@
 import string
+import time
 
 alphabet = " "+string.ascii_lowercase 
 
 positions = dict(zip(alphabet,list(range(0,27,1))))
 
-message = "hi my name is caesar"
+message = "teste um dois tres quatro cinco"
 
 def encoding(message,key=1):
     encoding_list = []
@@ -17,21 +18,24 @@ def encoding(message,key=1):
     
     return encoded_string
 
-'''
-def encoding(message):
 
-    encoded_message = ""
+def encoding2(message,key=1):
+
+    encoded_string = ""
 
     for i, char in enumerate(message):
         result = (positions[char]+1) % 27
-        encoded_message += alphabet[result]
-    return encoded_message
-'''
+        encoded_string += alphabet[result]
+    return encoded_string
+
+t0 = time.time()
 
 encoded_message = encoding(message,key = 3)
-    
 decoded_message = encoding(encoded_message,-3)
 
-print(encoded_message)
+t1 = time.time()
 
+print(t1-t0)
+
+print(encoded_message)
 print(decoded_message)
